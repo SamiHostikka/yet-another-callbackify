ceabf074c4b328626368aeeed6176319732d21be
hstkk <hostikka@gmail.com>
2017-09-04 21:12:01 +0300

    Add wrapper impl

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export default function wrapper(fn: Function) {
-    return () => {};
-};
+export default function wrapper(fn: (event: any) => Promise) {
+    return async (event: any, ctx: any, cb: AWSLambda.Callback) => {
+        try {
+            const result = await fn(event);
+            return cb(undefined, result);
+        } catch (error) {
+            return cb(error, undefined);
+        }
+    };
+}
